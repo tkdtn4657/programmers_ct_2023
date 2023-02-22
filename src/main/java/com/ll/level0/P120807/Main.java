59146c6def6c7a0d252e08c3e2df285d3ed391db
tkdtn4657 <tkdtn101226@gmail.com>
2023-02-23 02:51:42 +0900

    문제 p120807 풀이

---
+++ src/main/java/com/ll/level0/P120807/Main.java
@@ -0,0 +1,10 @@
+package com.ll.level0.P120807;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(new Solution().solution(2, 3)); // -1
+       // System.out.println(new Solution().solution(11, 11)); // 1
+        //System.out.println(new Solution().solution(7, 99)); // -1
+    }
+}
+
