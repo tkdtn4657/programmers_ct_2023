7c62c2f1dbaceb9e9161250681347fffe85b69bc
tkdtn4657 <tkdtn101226@gmail.com>
2023-02-23 10:32:16 +0900

    RED단계 ì

--- src/test/java/com/ll/level0/p120829/Tests.java
+++ src/test/java/com/ll/level0/p120829/Tests.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Tests {
+    @Test
+    @DisplayName("55일때 1을 출력하는 값")
 
+    void t1(){
+        assertThat(new Solution().solution(55)).isEqualTo(1);
+    }
 
 }
