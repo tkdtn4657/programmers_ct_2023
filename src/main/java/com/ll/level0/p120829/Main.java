10873bc39d15be35345df3d0b48bc8a650a54ef3
tkdtn4657 <tkdtn101226@gmail.com>
2023-02-23 10:29:28 +0900

    복습을 위한 초기화

--- src/main/java/com/ll/level0/p120829/Main.java
+++ src/main/java/com/ll/level0/p120829/Main.java
@@ -8,24 +8,7 @@ public class Main {
 
 class Solution {
     public int solution(int angle) {
-        if( angle == 90) return 2;
-        else if( angle == 180) return 4;
-        else if( angle > 90) return 3;
 
-        return 1;
-
-        //        if(0 < angle && angle < 90){
-//            return 1;
-//        }
-//        else if(angle == 90){
-//            return 2;
-//        }
-//        else if(90 < angle && angle < 180){
-//            return 3;
-//        }
-//        else if(angle == 180){
-//            return 4;
-//        }
-//        return -1;
+        return 0;
     }
 }
